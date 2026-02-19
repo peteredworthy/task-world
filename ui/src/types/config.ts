ece57a1a8631ec31051f6f880da0f0d34f29ec19
Peter Edworthy <peter@edworthy.org>
2026-02-19 16:34:37 -0500

    Align GlobalConfig type with /api/config response schema

--- ui/src/types/config.ts
+++ ui/src/types/config.ts
@@ -1,7 +1,4 @@
 export interface GlobalConfig {
-  db_path: string;
-  active_agent_types: string[];
-  max_recent_runs: number;
   dashboard_refresh_interval_seconds: number;
   dashboard_max_recent_runs: number;
   agents_openhands_url: string | null;
